c4fb489972043f68e688f3f17b86e709f3b4bd7f
dhruvstoretransform-dev <dhruvstoretransform@gmail.com>
2026-07-15 17:04:38 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,17 +4,23 @@ import Philosophy from "../components/Philosophy.jsx";
 import Mission from "../components/Mission.jsx";
 import Values from "../components/values.jsx";
 import Stats from "../components/Stats.jsx";
+import Navbar from "../Common/Navbar.jsx";
+import Footer from "../Common/Footer.jsx";
+import CTA from "../components/CTA.jsx";
 
 
 function About() {
     return (
         <>
+            <Navbar />
             <AboutHero />
             <Leadership />
             <Philosophy/>
             <Mission/>
             <Values/>
             <Stats/>
+            <CTA />
+            <Footer />
         </>
     );
 }
